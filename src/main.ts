d9a31b14472bcbf3b892cc9256e3bf4489c25809
Konrad Reczko <reczkok@gmail.com>
2025-10-03 16:32:10 +0200

    make the light world space, add edge normals and make slime aware of the terrain

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import './style.css';
 
 import tgpu from 'typegpu';
-import * as d from 'typegpu/data';
 import * as wf from 'wayfare';
 import { createCameraRig } from './cameraRig.ts';
 import { createChamber } from './chamber.ts';
 import { createInputManager } from './inputManager.ts';
 import { createTerrarium } from './terrarium.ts';
-
-const floorMesh = wf.createRectangleMesh({
-  width: d.vec3f(10, 0, 0),
-  height: d.vec3f(0, 0, -10),
-});
 
 async function initGame() {
   const root = await tgpu.init({
@@ -35,18 +29,11 @@ async function initGame() {
   window.addEventListener('resize', () => resizeCanvas(canvas));
 
   // Main menu dismiss
-  document.querySelector("#startButton")?.addEventListener("click", () => {
-    document.getElementById("titleScreen")?.classList.add("hidden");
+  document.querySelector('#startButton')?.addEventListener('click', () => {
+    document.getElementById('titleScreen')?.classList.add('hidden');
   });
 
   const world = engine.world;
-
-  // Floor
-  world.spawn(
-    wf.MeshTrait(floorMesh),
-    wf.TransformTrait({ position: d.vec3f(0, -5, -10) }),
-    ...wf.BlinnPhongMaterial.Bundle({ albedo: d.vec3f(0.7, 0.5, 0.3) }),
-  );
 
   // Attaches input controls to the canvas
   createInputManager(world, canvas);
