6a2c8dffaac0646152544a48c190727c5a3ba4b2
Iwo Plaza <iwoplaza@gmail.com>
2025-10-06 02:00:50 +0200

    More of the ending cutscene

--- src/chamber.ts
+++ src/chamber.ts
@@ -1,20 +1,28 @@
-import { trait, type World } from 'koota';
+import { trait, type Entity, type World } from 'koota';
 import * as d from 'typegpu/data';
 import * as wf from 'wayfare';
 import { quatn } from 'wgpu-matrix';
 import type { FoggyMaterial } from './foggyMaterial.ts';
+import { endingState, FALLING_STEP } from './endingState.ts';
 
-const [chamberMesh, fanMesh] = await Promise.all([
+const [chamberMesh, chamberBrokenMesh, fanMesh] = await Promise.all([
   wf.meshAsset({ url: 'assets/models/chamber.obj' }).preload(),
+  wf.meshAsset({ url: 'assets/models/chamber_broken.obj' }).preload(),
   wf.meshAsset({ url: 'assets/models/fan.obj' }).preload(),
 ]);
 
 const FanTag = trait();
 
 export function createChamber(world: World, foggyMaterial: FoggyMaterial) {
-  world.spawn(
+  const chamberEntity = world.spawn(
     wf.MeshTrait(chamberMesh),
     wf.TransformTrait({ position: d.vec3f(0, 0, 0), scale: d.vec3f(1) }),
+    ...foggyMaterial.Bundle({ albedo: d.vec3f(1) }),
+  );
+
+  const chamberBrokenEntity = world.spawn(
+    wf.MeshTrait(chamberBrokenMesh),
+    wf.TransformTrait({ position: d.vec3f(0, 0, 0), scale: d.vec3f(0) }),
     ...foggyMaterial.Bundle({ albedo: d.vec3f(1) }),
   );
 
@@ -37,6 +45,13 @@ export function createChamber(world: World, foggyMaterial: FoggyMaterial) {
           transform.rotation,
         );
       });
+
+      if (endingState.step >= FALLING_STEP && chamberEntity.isAlive()) {
+        chamberEntity.destroy();
+        chamberBrokenEntity.get(wf.TransformTrait)!.scale.x = 1;
+        chamberBrokenEntity.get(wf.TransformTrait)!.scale.y = 1;
+        chamberBrokenEntity.get(wf.TransformTrait)!.scale.z = 1;
+      }
     },
   };
 }
